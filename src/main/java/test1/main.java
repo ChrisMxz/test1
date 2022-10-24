0c4498916d94c040c502eb23979ec80e4e3bcb1c
David <cristhegood15@gmail.com>
2022-10-24 10:26:10 -0500

    3r commit

--- src/main/java/test1/main.java
+++ src/main/java/test1/main.java
@@ -4,16 +4,15 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 
-		
-		//nuevo de branch
+		// nuevo de branch
 		System.out.println("Nuevo branch DEV");
-		
-		//Nuevo commit 
+
+		// Nuevo commit
 		System.out.println("2do commit desde rama dev");
-		
-		
+
+		// Nuevo commit
+		System.out.println("3er commit desde rama dev");
 
 	}
 
